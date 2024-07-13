adf6b9217a08da3b4bbd9904a00769b9e34344e6
Templated User <templateduser@dummyemailX123.com>
2024-07-13 11:09:41 +0100

    Baseline Baby

---
+++ administrator/cache/com_content/e92a2db5ca4fef59c9c94813c215fe5c-cache-com_content-c1224ed3166602432b023e180709e6fc.php
@@ -0,0 +1,59 @@
+<?php die("Access Denied"); ?>#x#a:5:{s:4:"body";s:3585:"<div class="com-content-article item-page">
+    <meta itemprop="inLanguage" content="en-GB">
+        <div class="page-header">
+        <h1> Home </h1>
+    </div>
+    
+    
+        <div class="page-header">
+        <h2>
+            Introduction        </h2>
+                            </div>
+        
+        
+            <dl class="article-info text-muted">
+
+            <dt class="article-info-term">
+                            Details                    </dt>
+
+                    <dd class="createdby">
+    <span class="icon-user icon-fw" aria-hidden="true"></span>
+                    Written by: <span>Webmaster</span>    </dd>
+        
+        
+                    <dd class="category-name">
+    <span class="icon-folder-open icon-fw" aria-hidden="true"></span>                        Category: <a href="/index.php?option=com_content&amp;view=category&amp;id=2&amp;Itemid=101">Uncategorised</a>    </dd>
+        
+        
+                    <dd class="published">
+    <span class="icon-calendar icon-fw" aria-hidden="true"></span>
+    <time datetime="2024-06-26T14:05:49+00:00">
+        Published: 26 June 2024    </time>
+</dd>
+        
+    
+            
+        
+                    <dd class="hits">
+    <span class="icon-eye icon-fw" aria-hidden="true"></span>
+    <meta content="UserPageVisits:19">
+    Hits: 19</dd>
+            </dl>
+    
+    
+        
+                                                <div class="com-content-article__body">
+        <p>The credentials for this application are:</p>
+<p>username: webmaster</p>
+<p>password: mnbcxz098321QQZZ</p>
+<p>This is an example website deployed using the Agile Deployment Toolkit <a href="https://www.github.com/wintersys-projects">www.github.com/wintersys-projects</a> . </p>
+<p>What this shows is how a pre-configured website can be deployed using the ADT without any need for the person deploying the website to actually do any of the steps to build it for example, choosing and configuring templates, components, modules and plugins.</p>
+<p>This is supposed to make it possible for one "company" to develop applications and another "company" to deploy them.</p>
+<p>The first company can be the experts in building applications to meet the business needs of the second company and beyond that companies if there are companies who have similar business requirements. </p>
+<p>This application is a very simple example of a social network installation and it is built using community builder from Joomlapolis <a href="https://www.joomlapolis.com">www.joomlapolis.com</a>. I didn't spend any time making it look pretty or doing additional configurations it's quite simply an install of community builder. You could spend time building a highly functional and highly developed installation of community builder which people might be eager to deploy for their own uses.</p>
+<p>One of the advantages of my approach for community builder is that as I understand it image and media offloading to an S3 compatible datastore isn't possible with community builder (at the moment). This toolkit makes that possible so that you can have TB of user media rather than the only GB of user media if you use the disk space provided with your hosting package. The way that media offloading is made possible is by making use of s3fs or alternatively goofys to mount the S3 bucket as a local file system directory (usually /var/www/html/images).</p>
+<p>Peformance of an S3 mount is acceptable although not as performant as a POSIX filesystem would be. </p>     </div>
+
+        
+                                        </div>
+";s:4:"head";a:13:{s:5:"title";s:12:"Introduction";s:11:"description";s:65:"Joomla! - the dynamic portal engine and content management system";s:4:"link";s:0:"";s:8:"metaTags";a:2:{s:10:"http-equiv";a:1:{s:12:"content-type";s:24:"text/html; charset=utf-8";}s:4:"name";a:2:{s:6:"rights";N;s:6:"author";s:9:"Webmaster";}}s:5:"links";a:0:{}s:11:"styleSheets";a:0:{}s:5:"style";a:0:{}s:7:"scripts";a:0:{}s:6:"script";a:0:{}s:6:"custom";a:0:{}s:10:"scriptText";a:0:{}s:13:"scriptOptions";a:0:{}s:12:"assetManager";a:2:{s:13:"registryFiles";a:8:{i:0;s:30:"media/vendor/joomla.asset.json";i:1;s:30:"media/system/joomla.asset.json";i:2;s:30:"media/legacy/joomla.asset.json";i:3;s:46:"media/plg_system_guidedtours/joomla.asset.json";i:4;s:35:"media/com_content/joomla.asset.json";i:5;s:38:"templates/cassiopeia/joomla.asset.json";i:6;s:41:"media/plg_behaviour_compat/es5.asset.json";i:7;s:45:"media/plg_behaviour_compat/removed.asset.json";}s:6:"assets";a:0:{}}}s:13:"mime_encoding";s:9:"text/html";s:7:"pathway";a:0:{}s:6:"module";a:0:{}}
